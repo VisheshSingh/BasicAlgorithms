f5b43b41deb7e87c042bbe1cf6929746c85b31a6
Vishesh <visheshsingh16@gmail.com>
2018-11-02 19:20:51 -0400

    Factorial of a number

---
+++ factorialze.js
@@ -0,0 +1,26 @@
+/* Return the factorial of the provided integer.
+
+If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+Factorials are often represented with the shorthand notation n!
+
+For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+Only integers greater than or equal to zero will be supplied to the function. */
+
+function factorialize(num) {
+  var count = num;
+  if (count == 0) {
+    return 1;
+  }
+
+  for (var i = 1; i < count; i++) {
+    num = num * i;
+  }
+  return num;
+}
+
+console.log(factorialize(5));
+console.log(factorialize(10));
+console.log(factorialize(20));
+console.log(factorialize(0));
