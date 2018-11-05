421afecce02dc424f6b2bf563cd4b57208843599
Vishesh <visheshsingh16@gmail.com>
2018-11-04 19:35:00 -0500

    Spinal Tape Case

---
+++ SpinalCase.js
@@ -0,0 +1,16 @@
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  //   Split the string at one of the following conditions (converted to an array)
+
+  // a whitespace character [\s] is encountered
+  // underscore character [_] is encountered
+  // or is followed by an uppercase letter [(?=[A-Z])]
+  // Join the array using a hyphen (-)
+  // Lowercase the whole resulting string
+  return str
+    .split(/\s|_|(?=[A-Z])/)
+    .join("-")
+    .toLowerCase();
+}
+
+console.log(spinalCase("This Is Spinal Tap"));
